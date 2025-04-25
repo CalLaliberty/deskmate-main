e3c520ebaa37b183860e48efc9e3b88922b7a995
CalLaliberty <claliberty12@gmail.com>
2025-04-25 14:53:03 -0400

    Update file_categories.py
    - Changed name to audio and added .m4a file types

--- utils/file_categories.py
+++ utils/file_categories.py
@@ -4,7 +4,7 @@ FILE_CATEGORIES = {
     "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp"],
     "Documents": [".pdf", ".docx", ".txt", ".xlsx", ".pptx"],
     "Videos": [".mp4", ".avi", ".mov", ".mkv"],
-    "Music": [".mp3", ".wav", ".aac"],
+    "Audio": [".mp3", ".wav", ".aac",".m4a"],
     "Archives": [".zip", ".rar", ".tar", ".gz"],
     "Applications": [".exe", ".msi"],
     "Others": []
